1ca62eecb0e29bf0ffec04eb31c7ee820c2321d6
smartsabari1303 <35765714+smartsabari1303@users.noreply.github.com>
2018-02-02 14:23:02 +0530

    Create s66.C

---
+++ s66.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+ {	
+char sss;
+printf("enter the variable");
+scanf("%c",&sss);
+if(sss=='a'||sss=='e'||sss=='i'||sss=='o'||sss=='u')
+{
+printf("the given variable is vowel");
+}
+else
+{
+printf("the given variable is constant");
+}
+	return 0;
+}
