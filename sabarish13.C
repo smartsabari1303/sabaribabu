bca03812757b47a22ddfc1323c861cf2f76650b5
smartsabari1303 <35765714+smartsabari1303@users.noreply.github.com>
2018-02-02 14:12:30 +0530

    Create sabarish13.C

---
+++ sabarish13.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a>1)
+printf("the number is positive");
+else
+printf("the number is negative");
+else
+printf("the number is zero");
+return 0;
+}
