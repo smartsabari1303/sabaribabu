75b3d866e0643fddc69f2c9eecb247cbc94a28b1
smartsabari1303 <35765714+smartsabari1303@users.noreply.github.com>
2018-02-05 16:33:34 +0530

    Create SABARI16.C

---
+++ SABARI16.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,sum;
+int array[N,K];
+printf("\n enter the arrays");
+scanf("%d",&array);
+for(i=0;i<=n;i++)
+{
+sum=n+k;
+}
+printf("\n sum of arrays:%d");
+getch();
+return 0;
+}
