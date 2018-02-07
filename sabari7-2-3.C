22190c033bd66b93e4d212be0bc1ba8b54cd5722
smartsabari1303 <35765714+smartsabari1303@users.noreply.github.com>
2018-02-07 16:24:44 +0530

    Create sabari7-2-3.C

---
+++ sabari7-2-3.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,c=0;
+    scanf("%d",&n);
+    while(n!=0)
+    {   
+        n/=10;
+        c=c+1;
+     }
+     printf("%d",c);
+     getch();
+     return 0;
+}
+    
