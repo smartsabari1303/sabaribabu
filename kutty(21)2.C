3d3561c234c2ae3f67f34fa5a82c4dedda1d79eb
smartsabari1303 <35765714+smartsabari1303@users.noreply.github.com>
2018-02-21 16:06:29 +0530

    Create kutty(21)2.C

---
+++ kutty(21)2.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+int a[10],i,n,max=0;
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+  scanf("%d",&a[i]);
+ }
+ max=a[0];
+for(i=0;i<n;i++)
+{
+ if(a[i]>max)
+ {
+  max=a[i];
+ }
+}
+printf("%d",max);
+getch();
+	return 0;
+}
