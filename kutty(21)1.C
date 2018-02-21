e6c887acb26998076437f282ef0ccf9c527df0ab
smartsabari1303 <35765714+smartsabari1303@users.noreply.github.com>
2018-02-21 15:52:27 +0530

    Create kutty(21)1.C

---
+++ kutty(21)1.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+void main()
+{
+	char a[50];
+	int i,n,count=0;
+	printf("ENTER THE INPUT STRING:\n");
+	gets(a);
+	n=strlen(a);
+	printf("THE LENGTH OF STRING IS %d",n);
+	for(i=0;i<n;i++)
+	{
+		if(isalpha(a[i]))
+		{
+			count++;
+		}
+	}
+	printf("\nTHE NO.OF.CHARCTERS IN THE GIVEN STRING IS %d",count);
+  getch();
+  return 0;
+}
