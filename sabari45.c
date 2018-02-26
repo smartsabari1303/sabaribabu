10074a59bcaa332f66603c7cd367214472482bda
smartsabari1303 <35765714+smartsabari1303@users.noreply.github.com>
2018-02-26 16:31:37 +0530

    Create sabari45.c

---
+++ sabari45.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,i,a=0;
+    scanf("%d",&n);
+    for(i=1;i<=10;i++)
+    {
+        if(n==i)
+        a++;
+    }
+        if(a==1)
+    
+        printf("yes");
+        else
+        printf("no");
+    getch();
+    
+ 
+
+    return 0;
+   }
