0c73b76b3547d82916015d0c132a63b39129cdbc
smartsabari1303 <35765714+smartsabari1303@users.noreply.github.com>
2018-02-26 16:19:38 +0530

    Create sabari42.c

---
+++ sabari42.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	int i;
+	char s1[10],s2[10];
+	gets(s1);
+	gets(s2);
+	for(i=0;s1[i]!='\0';i++)
+	{
+		if(s1[i]>s2[i])
+		{
+			printf("%s is greater",s1);
+			break;
+		}
+		else if(s1[i]<s2[i])
+		{
+			printf("%s is greater",s2);
+			break;
+		}
+		else
+		{
+			printf("both are equal");
+			break;
+		}
+	}
+	return 0;
+}
