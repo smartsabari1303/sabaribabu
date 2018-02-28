bc21295540189278fa481065f7b536291246e086
smartsabari1303 <35765714+smartsabari1303@users.noreply.github.com>
2018-02-28 15:40:29 +0530

    Create sabari1(28).c

---
+++ sabari1(28).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+	int num;
+	printf("enter the number:");
+	scanf("%d",&num);
+	int result=num+1;
+	printf("\n%d",result);
+  getch();
+	return 0;
+}
