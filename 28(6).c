e908f0e2ca92a579eb1d38f015718a9e6e913f22
smartsabari1303 <35765714+smartsabari1303@users.noreply.github.com>
2018-02-28 16:08:11 +0530

    Create 28(6).c

---
+++ 28(6).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+    int n,rev=0,rem;
+    scanf("%d",&n);
+    while(n!='\0')
+    {
+        rem=n%10;
+        rev=(rev*10)+rem;
+        n=n/10;
+    }
+    n=rev;
+    while(n!='\0')
+    {
+        rem=n%10;
+        printf("%d ",rem);
+        n=n/10;
+        getch();
+	}
+	return 0;
+}
