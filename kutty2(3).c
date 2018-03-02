eaa432f095387716e7eaa69ad0b67bc58786c57f
smartsabari1303 <35765714+smartsabari1303@users.noreply.github.com>
2018-03-02 13:16:31 +0530

    Create kutty2(3).c

---
+++ kutty2(3).c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+ #include<conio.h>
+
+int main()
+
+{
+
+int x,y,temp;
+
+printf("\n enter two numbers to swap:");
+
+scanf("%d %d",&x,&y);
+
+printf("\n before swapping:");
+
+printf("\nx=%d",x);
+
+printf("\ny=%d",y);
+
+temp=x;
+
+x=y;
+
+y=temp;
+
+printf("\n after swapping:");
+
+printf("\nx=%d",x);
+
+printf("\ny=%d",y);
+
+return 0;
+
+}#include <stdio.h>
+
+ 
+
+int main(void) 
+
+{
+
+int x,y,temp;
+
+printf("\n enter two numbers to swap:");
+
+scanf("%d %d",&x,&y);
+
+printf("\n before swapping:");
+
+printf("\nx=%d",x);
+
+printf("\ny=%d",y);
+
+temp=x;
+
+x=y;
+
+y=temp;
+
+printf("\n after swapping:");
+
+printf("\nx=%d",x);
+
+printf("\ny=%d",y);
+getch();
+
+return 0;
+
+}
