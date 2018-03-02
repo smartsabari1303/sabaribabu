33c7d518ac85a9d017f249695fe1c3fb4397e3ad
smartsabari1303 <35765714+smartsabari1303@users.noreply.github.com>
2018-03-02 13:42:10 +0530

    Create kutty10(3).c

---
+++ kutty10(3).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+	char str[50];
+	int i;
+	printf("enter the string:");
+	scanf("%[^\n]s",str);
+	if(printf("\n%s",str))
+  getch();
+	{
+		
+	}
+	return 0;
+}
