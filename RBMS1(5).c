14f795ed36719e1e21dd9cf391b497859f3d5bdb
smartsabari1303 <35765714+smartsabari1303@users.noreply.github.com>
2018-03-05 15:51:10 +0530

    Create RBMS1(5).c

---
+++ RBMS1(5).c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+char string[50];
+int i=0,count=0,flag=0;
+printf("\n enter the given string:");
+scanf("%s",string);
+while(string[i]!='\0')
+{
+if(string[i]=='a'||string[i]=='e'||string[i]=='i'||string[i]=='o'||string[i]=='u'||string[i]=='A'||string[i]=='E'||string[i]=='I'||string[i]=='O'||string[i]=='U')
+flag=1;
+i++;
+}
+if(flag==1)
+{
+	printf("\nyes");
+}
+else
+{
+	printf("\n no");
+  getch();
+}
+return 0;
+}
