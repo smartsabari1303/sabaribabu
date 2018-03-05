1680bea668f37692ca35f59cbf4fef5361aac2a1
smartsabari1303 <35765714+smartsabari1303@users.noreply.github.com>
2018-03-05 15:58:49 +0530

    Create RBMS3(5).C

---
+++ RBMS3(5).C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+	float num;
+	int x;
+	printf("\n enter the num:");
+	scanf("%f",&num);
+	x=(int)(num+0.5);
+	printf("\n%d",x);
+  getch();
+	return 0;
+}
