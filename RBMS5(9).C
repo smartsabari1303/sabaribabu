7091216fd8551e87a2ddf780995d657b04140b1b
smartsabari1303 <35765714+smartsabari1303@users.noreply.github.com>
2018-03-09 13:39:07 +0530

    Create RBMS5(9).C

---
+++ RBMS5(9).C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	float p,r,result;
+	int si,n;
+	scanf("%f %d %f",&p,&n,&r);
+	result=((p*n*r)/100);
+	si=floor(result);
+	printf("\n%d",si);
+  getch();
+	return 0;
+}
