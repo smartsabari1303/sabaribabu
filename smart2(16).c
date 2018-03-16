682a0944fe6ef1056b83e4d6abec313e5610330a
smartsabari1303 <35765714+smartsabari1303@users.noreply.github.com>
2018-03-16 15:51:41 +0530

    Create smart2(16).c

---
+++ smart2(16).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+	int n,k,i,result=1;
+	scanf("%d %d",&n,&k);
+	for(i=1;i<=k;i++)
+	{
+		result=result*n;	
+	}
+	printf("%d",result);
+  getch();
+	return 0;
+}
