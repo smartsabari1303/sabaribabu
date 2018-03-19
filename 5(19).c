7e6eff3e7e1024947f14469225b7cf8e52c1ca0a
smartsabari1303 <35765714+smartsabari1303@users.noreply.github.com>
2018-03-19 16:03:06 +0530

    Create 5(19).c

---
+++ 5(19).c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,n,k,array[50],temp;
+	scanf("%d %d",&n,&k);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&array[i]);
+	}
+	for(i=1;i<=n;i++)
+	{
+		if(array[i]>array[i+1])
+		{
+			temp=array[i];
+			array[i]=array[i+1];
+			array[i+1]=temp;
+		}
+	}
+	for(i=1;i<=n;i++)
+	{
+		if(i==k-1)
+		{
+			printf("%d",array[i]);
+      getch();
+		}
+	}
+			
+	return 0;
+}
