91be2a93f978b74c957c64c862dbf1401f2423bb
smartsabari1303 <35765714+smartsabari1303@users.noreply.github.com>
2018-03-19 15:53:04 +0530

    Create 2(19).c

---
+++ 2(19).c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,n,k,array[50],flag=0;
+	scanf("%d %d",&n,&k);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&array[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(array[i]==k)
+		{
+			flag=1;
+			break;
+		}
+	}
+	if(flag==1)
+	{
+		printf("\nyes");
+	}
+	else
+	{
+		printf("\nno");
+    getch();
+	}
+	return 0;
+}
